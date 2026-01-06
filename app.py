a2ee525d63274d2c09fabdc5efba6e6f4e236105
Jiří Pavlík <jiri.pavlik@gendigital.com>
2026-01-06 16:31:33 +0100

    Add basic logging

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import string
 import random
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'awdjklajwdiwaiojdiqojdwkqjdklajdioqjdioqdjasnfhrenfri'
@@ -14,6 +19,21 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'index'
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    file_handler = RotatingFileHandler('logs/app.log', maxBytes=102400, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Logging configured')
 
 def generate_group_code():
     chars = string.ascii_uppercase + string.digits
@@ -57,8 +77,10 @@ def login():
     user = User.query.filter_by(username=username).first()
     if user and check_password_hash(user.password, password):
         login_user(user)
+        app.logger.info(f"User '{username}' logged in successfully.")
         return redirect(url_for('dashboard'))
     else:
+        app.logger.info(f"Failed login attempt for user '{username}'.")
         flash('Špatné jméno nebo heslo.', 'error')
         return redirect(url_for('index'))
 
@@ -70,12 +92,14 @@ def register():
     reg_type = request.form.get('reg_type')
     
     if User.query.filter_by(username=username).first():
+        app.logger.info(f"Failed registration for username '{username}'. Already exists.")
         flash('Uživatel s tímto přihlašovacím jménem/id již existuje.', 'error')
         return redirect(url_for('index'))
 
     hashed_pw = generate_password_hash(password, method='pbkdf2:sha256')
     
     if reg_type == 'create':
+        app.logger.info(f"Creating new group {group_name} and user '{username}'.")
         group_name = request.form.get('group_name')
         if not group_name:
             flash('Zadej název rodiny.', 'error')
@@ -86,20 +110,25 @@ def register():
         new_group = Group(name=group_name, join_code=code)
         db.session.add(new_group)
         db.session.commit()
+        app.logger.info(f"New group {group_name} and user '{username}' successfuly initialized.")
         new_user = User(username=username, display_name=display_name, password=hashed_pw, group=new_group)
         
     elif reg_type == 'join':
+        app.logger.info(f"User '{username}' joining a group.")
         join_code = request.form.get('join_code').upper()
         group = Group.query.filter_by(join_code=join_code).first()
         if not group:
+            app.logger.info(f"User '{username}' failed to join a group. Invalid code")
             flash('Neplatný kód rodiny.', 'error')
             return redirect(url_for('index'))
         new_user = User(username=username, display_name=display_name, password=hashed_pw, group=group)
+        app.logger.info(f"User '{username}'successfuly joined group '{group.name}'.")
     else:
         return redirect(url_for('index'))
 
     db.session.add(new_user)
     db.session.commit()
+    app.logger.info(f"New user '{username}' saved.")
     login_user(new_user)
     return redirect(url_for('dashboard'))
 
@@ -124,6 +153,7 @@ def add_wish():
         new_wish = Wish(text=text, url=url, owner=current_user)
         db.session.add(new_wish)
         db.session.commit()
+        app.logger.info(f"New wish created by {current_user.username}.")
     return redirect(url_for('dashboard'))
 
 @app.route('/delete_wish/<int:wish_id>')
@@ -133,6 +163,7 @@ def delete_wish(wish_id):
     if wish.owner == current_user:
         db.session.delete(wish)
         db.session.commit()
+        app.logger.info(f"Wish {wish_id} deleted by {current_user.username}.")
     return redirect(url_for('dashboard'))
 
 @app.route('/toggle_reserve/<int:wish_id>')
@@ -146,9 +177,11 @@ def toggle_reserve(wish_id):
     if wish.reserved_by is None:
         wish.reserved_by = current_user.id
         db.session.commit()
+        app.logger.info(f"Wish {wish_id} reserved by {current_user.username}.")
     elif wish.reserved_by == current_user.id:
         wish.reserved_by = None
         db.session.commit()
+        app.logger.info(f"Wish {wish_id} unreserved by {current_user.username}.")
     return redirect(request.referrer or url_for('dashboard'))
 
 @app.route('/my_reservations')
